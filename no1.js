0ce82276625b69d57ee81d5a53a7a09ebd4c768d
Your Name <you@example.com>
2020-09-18 20:52:41 +0700

    evaluasi

---
+++ no1.js
@@ -0,0 +1,5 @@
+let santriIkhwan = ['sidik','rahmad','sofyan','aldi','kardi']
+const [ Lampung, Prabumulih,...rest] = santriIkhwan;
+console.log(Lampung);
+console.log(Prabumulih);
+console.log(rest);
